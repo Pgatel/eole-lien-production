dc9d9507fd5fb7b43fca6bc025617eab1c7bd2f7
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-03-01 17:29:13 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -21,3 +21,27 @@ def explore():
   print(eole_df.columns)
   eole_df = prepare_data(eole_df)
   print(eole_df.head())
+
+@anvil.server.callable
+def create_plots():
+  netflix_df, country_counts = prepare_netflix_data()
+
+  fig1 = go.Figure(
+    go.Scattergeo(
+      locations=sorted(netflix_df['country'].unique().tolist()), 
+      locationmode='country names',  
+      text = country_counts['counts'],
+      marker= dict(size= country_counts['counts'], sizemode = 'area')))
+
+  fig2 = go.Figure(go.Pie(
+    labels=netflix_df['type'], 
+    values=netflix_df['type'].value_counts()
+  ))
+  
+  fig3 = go.Figure(
+    go.Scatter(
+      x=netflix_df['date_added'].dt.year.value_counts().sort_index().index, 
+      y=netflix_df['date_added'].dt.year.value_counts().sort_index()
+    ))
+
+  return fig1, fig2, fig3
