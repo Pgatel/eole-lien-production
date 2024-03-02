d1ec53a77c569940816d5b4121ee42058e3a8f14
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-03-02 06:54:52 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -5,43 +5,35 @@ import anvil.files
 from anvil.files import data_files
 import anvil.server
 import pandas as pd
+import plotly.graph_objects as go
 
 def csv_to_df(f):
   df = pd.read_csv(data_files[f], sep=';')
   return df
 
-def prepare_data(df):
-  df['Month'] = pd.to_datetime(df['Month'])
-  return df
+def prepare_data():
+  eole_df = csv_to_df()
+  eole_df['Month'] = pd.to_datetime(eole_df['Month'])
+  counts = pd.DataFrame(eole_df['Total (MWh)']).value_counts()
+  print(counts)
+  return eole_df, counts
 
 @anvil.server.callable
 def explore():
   eole_df = csv_to_df('Résumé-2021-2023-Court.csv')
-  print(eole_df.head())
+  print(eole_df)
   print(eole_df.columns)
-  eole_df = prepare_data(eole_df)
-  print(eole_df.head())
+  eole_df = prepare_data()
+  print(eole_df)
+  return eole_df
 
 @anvil.server.callable
-def create_plots():
-  netflix_df, country_counts = prepare_netflix_data()
-
-  fig1 = go.Figure(
-    go.Scattergeo(
-      locations=sorted(netflix_df['country'].unique().tolist()), 
-      locationmode='country names',  
-      text = country_counts['counts'],
-      marker= dict(size= country_counts['counts'], sizemode = 'area')))
-
-  fig2 = go.Figure(go.Pie(
-    labels=netflix_df['type'], 
-    values=netflix_df['type'].value_counts()
-  ))
-  
+def create_plots(df):
+  eole_df, counts = prepare_data()
   fig3 = go.Figure(
     go.Scatter(
-      x=netflix_df['date_added'].dt.year.value_counts().sort_index().index, 
-      y=netflix_df['date_added'].dt.year.value_counts().sort_index()
+      x=eole_df['Month'].dt.year.value_counts().sort_index().index, 
+      y=eole_df['Total (MWh)']
     ))
-
-  return fig1, fig2, fig3
+  return fig3
+  
