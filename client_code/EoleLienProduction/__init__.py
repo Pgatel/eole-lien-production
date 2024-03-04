752fa5e7b863074bba47b656780d082731316d10
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-03-04 13:48:37 +0000

    Created Logo-Eole-Lien-Vignette50.jpg and EoleLienProduction; Edited settings and ServerModule1; Deleted Panda

--- client_code/EoleLienProduction/__init__.py
+++ client_code/EoleLienProduction/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import PandaTemplate
+from ._anvil_designer import EoleLienProductionTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -6,7 +6,7 @@ from anvil.tables import app_tables
 import anvil.server
 import plotly.graph_objects as go
 
-class Panda(PandaTemplate):
+class EoleLienProduction(EoleLienProductionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -14,5 +14,5 @@ class Panda(PandaTemplate):
     # Any code you write here will run before the form opens.
     fig = anvil.server.call('create_plots')
     self.plot_1.figure = fig
-    self.plot_1.layout.yaxis.title = 'kWh'
-    
+
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -6,24 +6,24 @@ from anvil.files import data_files
 import anvil.server
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
 @anvil.server.callable
 def create_plots():
   file = data_files['Résumé-2021-2023-Court.csv']
   eole_df = pd.read_csv(file, sep=';')
   eole_df['Month'] = pd.to_datetime(eole_df['Month'], dayfirst=True)
-  figure = go.Figure(
-    go.Bar(
-      x=eole_df['Month'],
-      y=eole_df['Total (MWh)'],
-    ))
-  figure.update_layout(
-      margin=dict(t=10, b=30, l=10, r=10),
-      paper_bgcolor='#363636',
-      bargap=0.5,
-      plot_bgcolor='#363636',
-    font=dict(family='Raleway', color='white'))
-  figure.update_traces(marker_color='#abf7b1')
-  return figure
+     
+    # Make the plot!
+  fig2 = px.bar(eole_df, x="Month", y="Total (MWh)", color="Total (MWh)",
+                barmode="group",
+                text="Total (MWh)",
+                color_continuous_scale='blugrn',
+               )
+  fig2.update_layout(font_family='Arial', title_font_size=24,
+                     margin={'l': 10, 'r': 10, 't': 10, 'b': 10})
+  fig2.update_xaxes(tickangle=90, dtick='M1')
+  fig2.update_yaxes(dtick=100, title='kWh')
+  return fig2
 
   
