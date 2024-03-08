2021e960c8ace1b506b179d815ed26eaae6ddabb
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-03-08 07:17:31 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -5,7 +5,6 @@ import anvil.files
 from anvil.files import data_files
 import anvil.server
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
 from datetime import date
 
@@ -15,24 +14,23 @@ def create_plots():
   data_list = [dict(row) for row in data]
   eole_df = pd.DataFrame(data_list)
   eole_df.set_index('Month', inplace=True)
-       
-  # Make the plot!
-  lt_production = eole_df.itertuples(index=False)
-  print(lt_production)
-  lc_production = eole_df['Production']
-  print(lc_production)
+  
   st_production = [f'{prod:8.3f}' for prod in eole_df['Production'].to_list()]
-  sc_production = [prod for prod in lc_production]
-  fig2 = px.bar(eole_df, x=eole_df.index, y="Production", color=sc_production,
-                barmode="group",
-                text=st_production,
-                color_continuous_scale='mint',
-               )
-  fig2.update_layout(font_family='Arial', title_font_size=24,
-                     margin={'l': 10, 'r': 10, 't': 10, 'b': 10})
-  fig2.update_xaxes(tickangle=90, dtick='M1')
-  fig2.update_yaxes(dtick=100, title='MWh')
-  return fig2
+  eole_df = eole_df.rename(columns={'Production': 'MWh', })
+
+  fig = px.bar(eole_df, x=eole_df.index, y="MWh", color='MWh',
+               barmode="group",
+               text=st_production, hover_data={'MWh'},
+               color_continuous_scale='mint',
+              )
+  fig.update_traces(hovertemplate=('<b>%{y:8.3f} MWh</b>'))
+  fig.update_layout(font_family='Arial', title_font_size=24,
+                    margin={'l': 10, 'r': 10, 't': 10, 'b': 10}, 
+                    hovermode='x unified', hoverlabel=dict(bgcolor='white'),
+                   )
+  fig.update_xaxes(dtick='M1', tickformat='%b-%Y')
+  fig.update_yaxes(dtick=100, title='MWh')
+  return fig
 
 @anvil.server.callable
 def get_production(): 
