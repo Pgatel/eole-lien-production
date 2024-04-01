8ca87be93bd8a68d1350fa53964b38767efaee35
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-04-01 16:35:46 +0000

    Edited EoleLienProductionDay

--- client_code/EoleLienProductionDay/__init__.py
+++ client_code/EoleLienProductionDay/__init__.py
@@ -4,7 +4,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-import plotly.graph_objects as go
+from datetime import datetime, timedelta, date
+
 
 class EoleLienProductionDay(EoleLienProductionDayTemplate):
   def __init__(self, **properties):
@@ -12,5 +13,54 @@ class EoleLienProductionDay(EoleLienProductionDayTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    fig = anvil.server.call('create_plots')
+    to_day = date.today()
+    self.this_day = to_day.timetuple().tm_yday
+    self.this_year = to_day.year
+    self.max_day = self.this_day
+    self.max_year = self.this_year
+    min_date = datetime.strptime(self.date_picker_1.min_date, '%d-%m-%Y')
+    self.min_day = min_date.timetuple().tm_yday
+    self.min_year = min_date.year
+
+    self.date_picker_1.date = to_day.strftime('%d-%m-%Y')
+    self.date_picker_1.max_date = to_day.strftime("%Y-%m-%d")
+
+    fig = anvil.server.call('plot_day', self.this_day, self.this_year)
     self.plot_1.figure = fig
+
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    choosen_date = self.date_picker_1.date
+    self.this_year = choosen_date.year
+    self.this_day = choosen_date.timetuple().tm_yday
+    
+    fig = anvil.server.call('plot_day', self.this_day, self.this_year)
+    self.plot_1.figure = fig
+
+  def backward_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.this_day == self.min_day and self.this_year == self.min_year:
+      return
+    self.this_day -= 1
+    if self.this_day == 0:
+      self.this_day = 365
+      self.this_year -= 1
+    this_date = date(self.this_year, 1, 1) + timedelta(days=self.this_day - 1)
+    self.date_picker_1.date = this_date.strftime('%d-%m-%Y')
+    fig = anvil.server.call('plot_day', self.this_day, self.this_year)
+    self.plot_1.figure = fig
+
+  def forward_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.this_day == self.max_day and self.this_year == self.max_year:
+      return
+    self.this_day += 1
+    if self.this_day == 366:
+      self.this_day = 1
+      self.this_year += 1
+    this_date = date(self.this_year, 1, 1) + timedelta(days=self.this_day - 1)
+    self.date_picker_1.date = this_date.strftime('%d-%m-%Y')
+    fig = anvil.server.call('plot_day', self.this_day, self.this_year)
+    self.plot_1.figure = fig
+
